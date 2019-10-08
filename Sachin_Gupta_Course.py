ff8cad9233b2469513c019746723a07cf28326a0
Your Name <you@example.com>
2019-10-08 21:33:07 +0530

    Coursera

---
+++ Sachin_Gupta_Course.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+import pandas as pd
+
+
+# In[7]:
+
+
+import numpy as np
+
+
+# In[8]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
