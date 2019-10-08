952d09e2ee0d9b01c67fbcc3d950603bd084e28c
sachin-786 <56268073+sachin-786@users.noreply.github.com>
2019-10-08 21:36:47 +0530

    Update Sachin_Gupta_Course.py

--- Sachin_Gupta_Course.py
+++ Sachin_Gupta_Course.py
@@ -1,26 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[6]:
-
-
 import pandas as pd
-
-
-# In[7]:
-
-
 import numpy as np
-
-
-# In[8]:
-
-
 print('Hello Capstone Project Course!')
-
-
-# In[ ]:
-
-
-
-
